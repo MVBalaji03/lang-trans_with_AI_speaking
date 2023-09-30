40f82630eecaf86855db61b25366b6bcc6136de9
MVBalaji03 <balajims0221@gmail.com>
2023-09-30 16:07:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import nltk
+from googletrans import Translator
+from gtts import gTTS
+import os
+
+nltk.download("punkt")
+
+translator = Translator()
+
+while True:
+
+    print("                        Select the language to be Translated")
+    print("")
+    print("--------------------------")
+
+    print("1. Tamil")
+    print("2. Telugu")
+    print("3. Hindi")
+    print("4. Malayalam")
+    print("5. Kannada")
+    print("6. Exit from the loop")
+
+    print("--------------------------")
+
+    your_choice = int(input("enter your choice : "))
+
+    if your_choice >= 1 and your_choice <= 5:
+        input_text = input("enter the text : ")
+        detected_lang = translator.detect(input_text).lang
+
+        if your_choice == 1:
+            translated = translator.translate(input_text, dest="ta")
+        elif your_choice == 2:
+            translated = translator.translate(input_text, dest="te")
+        elif your_choice == 3:
+            translated = translator.translate(input_text, dest="hi")
+        elif your_choice == 4:
+            translated = translator.translate(input_text, dest="ml")
+        else:
+            translated = translator.translate(input_text, dest="kn")
+
+        print("original text({}):{}".format(detected_lang, input_text))
+
+        print("translated text ({}):{}".format(translated.dest, translated.text))
+
+        tts = gTTS(text=translated.text, lang=translated.dest)
+        tts.save("translated_audio.mp3")
+        os.system("start translated_audio.mp3")
+
+    elif your_choice == 6:
+        print("Exiting the loop")
+        break
+
+    else:
+        print("choose from 1-4 options")
